7397ea282bc4c52e2559b05580f256cc37808e8a
srn_sousa <sricardos@me.com>
2025-03-23 19:59:20 -0300

    Criação da classe de Menu 'Principal'

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,9 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosEpisodio;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverterDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import static java.lang.System.out;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,17 +17,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		//api-key: "ac51073f"
-		ConsumoApi consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=ac51073f");
-		System.out.println(json);
-
-		//O JSON recebido na requisição foi convertido pela classe Mapper na Classe DadosSerie (Record)
-		DadosSerie dados = new ConverterDados().obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-		json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
-		DadosEpisodio dadosEpisodio = new ConverterDados().obterDados(json, DadosEpisodio.class);
-		out.println(dadosEpisodio);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
 	}
 
